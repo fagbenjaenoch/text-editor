fa56c6a4be2ca3521773b95b671c1780c8e22770
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-23 11:26:56 +0100

    added file opening feature

--- edit.c
+++ edit.c
@@ -206,6 +206,20 @@ int getWindowSize(int *rows, int *cols)
 	}
 }
 
+/*** file i/o ***/
+
+void editorOpen()
+{
+	char *line = "Hello, world";
+	ssize_t linelen = 13;
+
+	E.row.size = linelen;
+	E.row.chars = malloc(linelen + 1);
+	memcpy(E.row.chars, line, linelen);
+	E.row.chars[linelen] = '\0';
+	E.numrows = 1;
+}
+
 /*** append buffer ***/
 
 struct abuf
@@ -239,25 +253,35 @@ void editorDrawRows(struct abuf *ab)
 	int y;
 	for (y = 0; y < E.screenrows; y++)
 	{
-		if (y == E.screenrows / 3)
+		if (y >= E.numrows)
 		{
-			char welcome[80];
-			int welcomelen = snprintf(welcome, sizeof(welcome), "qEdit editor --version %s", EDIT_VERSION);
-			if (welcomelen > E.screencols)
-				welcomelen = E.screencols;
-			int padding = (E.screencols - welcomelen) / 2;
-			if (padding)
+			if (y == E.screenrows / 3)
+			{
+				char welcome[80];
+				int welcomelen = snprintf(welcome, sizeof(welcome), "qEdit editor --version %s", EDIT_VERSION);
+				if (welcomelen > E.screencols)
+					welcomelen = E.screencols;
+				int padding = (E.screencols - welcomelen) / 2;
+				if (padding)
+				{
+					abAppend(ab, "~", 1);
+					padding--;
+				}
+				while (padding--)
+					abAppend(ab, " ", 1);
+				abAppend(ab, welcome, welcomelen);
+			}
+			else
 			{
 				abAppend(ab, "~", 1);
-				padding--;
 			}
-			while (padding--)
-				abAppend(ab, " ", 1);
-			abAppend(ab, welcome, welcomelen);
 		}
 		else
 		{
-			abAppend(ab, "~", 1);
+			int len = E.row.size;
+			if (len > E.screencols)
+				len = E.screencols;
+			abAppend(ab, E.row.chars, len);
 		}
 
 		abAppend(ab, "\x1b[K", 1);
@@ -368,6 +392,8 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.numrows = 0;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 }
@@ -376,6 +402,7 @@ int main()
 {
 	enableRawMode();
 	initEditor();
+	editorOpen();
 
 	while (1)
 	{
