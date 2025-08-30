5021d045bf13e9e663fe0e8a9744ffd5a4c563c4
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-08-30 21:15:27 +0100

    added horizontal scroll

--- edit.c
+++ edit.c
@@ -41,6 +41,7 @@ struct editorConfig
 {
 	int cx, cy;
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -339,6 +340,14 @@ void editorScroll()
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff)
+	{
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols)
+	{
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 /**
@@ -382,11 +391,13 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if (len < 0)
+				len = 0;
 			// You'd find a lot of the checks below, it is used to truncate the row if it is greater than the terminal column size
 			if (len > E.screencols)
 				len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1)
@@ -395,6 +406,7 @@ void editorDrawRows(struct abuf *ab)
 		}
 	}
 }
+
 /**
  * Main screen refresh function
  * Handles complete redraw of the editor screen
@@ -416,7 +428,7 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -444,10 +456,7 @@ void editorMoveCursor(int key)
 		}
 		break;
 	case ARROW_RIGHT:
-		if (E.cy != E.screencols - 1)
-		{
-			E.cx++;
-		}
+		E.cx++;
 		break;
 	case ARROW_UP:
 		if (E.cy != 0)
@@ -531,6 +540,7 @@ void initEditor()
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 
