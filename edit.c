9653ebae6139208af0e2a26e80c213379b4fcd80
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-09-03 14:13:10 +0100

    added tab support

--- edit.c
+++ edit.c
@@ -43,6 +43,7 @@ typedef struct erow
 struct editorConfig
 {
 	int cx, cy;
+	int rx;
 	int rowoff;
 	int coloff;
 	int screenrows;
@@ -240,6 +241,21 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx)
+{
+	int rx = 0;
+	int j;
+
+	for (j = 0; j < cx; j++)
+	{
+		if (row->chars[j] == '\t')
+			rx += (EDIT_TAB_STOP - 1) - (rx % EDIT_TAB_STOP);
+		rx++;
+	}
+
+	return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
 	int tabs = 0;
@@ -369,6 +385,12 @@ void abFree(struct abuf *ab)
  */
 void editorScroll()
 {
+	E.rx = 0;
+	if (E.cy < E.numrows)
+	{
+		E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+	}
+
 	if (E.cy < E.rowoff)
 	{
 		E.rowoff = E.cy;
@@ -378,13 +400,13 @@ void editorScroll()
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
-	if (E.cx < E.coloff)
+	if (E.rx < E.coloff)
 	{
-		E.coloff = E.cx;
+		E.coloff = E.rx;
 	}
-	if (E.cx >= E.coloff + E.screencols)
+	if (E.rx >= E.coloff + E.screencols)
 	{
-		E.coloff = E.cx - E.screencols + 1;
+		E.coloff = E.rx - E.screencols + 1;
 	}
 }
 
@@ -466,7 +488,7 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -599,6 +621,7 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.rx = 0;
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.numrows = 0;
